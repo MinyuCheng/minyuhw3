1ff11f1ad030e6cdebf6028f0a808eaca04a606f
Minyu Cheng <minyu.cheng@student.csulb.edu>
2020-02-26 11:58:16 -0800

    commit3

--- def.js
+++ def.js
@@ -1,30 +1,80 @@
 const org1_depts = [
- {
-  name:'accounting',
-  children:[
-  { name: 'accounting payable', children:[]},
-  { name: 'accounting receivable', children:[]},
-  ]
- },
-{
- name:'finance',
- children:[],
-} 
-]
+  {
+    name: "accounting",
+    children: [
+      { name: "accounting payable", children: [] },
+      { name: "accounting receivable", children: [] }
+    ]
+  },
+  {
+    name: "finance",
+    children: []
+  }
+];
 
 const org2_depts = [
- {
-  name:'accounting',
-  children:[
-   { name:'cash', children:[]}, {name:'check', children:[]},
-   {
-     name:'accounting receivable',
-     children:[{name:'cash', children:[]}, {name:'check', children:[]}],
-   }
-],
-},
-{
-  name:'finance',
-  children:[{name:'investment', children:[]}],
-},
-]
+  {
+    name: "accounting",
+    children: [
+      { name: "accounting payable", children: [] },
+      {
+        name: "accounting receivable",
+        children: [
+          { name: "cash", children: [] },
+          { name: "check", children: [] }
+        ]
+      }
+    ]
+  },
+  {
+    name: "finance",
+    children: [{ name: "investment", children: [] }]
+  }
+];
+
+const org1 = document.getElementById("org1");
+
+PrintDept(org1_depts);
+
+function PrintDept(depts) {
+  let output = "<ul>";
+
+  output += `<li>${depts[0].name}</li>`;
+
+  output += "<ul>";
+  output += `<li>${depts[0].children[0].name}</li>`;
+  output += `<li>${depts[0].children[1].name}</li>`;
+  output += "</ul>";
+
+  output += `<li>${depts[1].name}</li>`;
+  output += "</ul>";
+
+  org1.innerHTML = output;
+}
+
+const org2 = document.getElementById("org2");
+
+PrintDept2(org2_depts);
+
+function PrintDept2(depts) {
+  let output = "<ul>";
+
+  output += `<li>${depts[0].name}</li>`;
+
+  output += "<ul>";
+  output += `<li>${depts[0].children[0].name}</li>`;
+  output += `<li>${depts[0].children[1].name}</li>`;
+  output += "<ul>";
+  output += `<li>${depts[0].children[1].children[0].name}</li>`;
+  output += `<li>${depts[0].children[1].children[1].name}</li>`;
+  output += "</ul>";
+
+  output += "</ul>";
+
+  output += `<li>${depts[1].name}</li>`;
+  output += "<ul>";
+  output += `<li>${depts[1].children[0].name}</li>`;
+  output += "</ul>";
+
+  org2.innerHTML = output;
+}
